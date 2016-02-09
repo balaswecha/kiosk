33c4d7a9056321d9172ba3c18fd432772abc8784
Prateek Srivastava <prateeks@thoughtworks.com>
2016-02-09 12:13:27 +0530

    annotations will be disabled by default in videoPlayer + search-result is refactored.

--- src/search-result.js
+++ src/search-result.js
@@ -1,28 +1,5 @@
 window.$ = window.jQuery = require('./jquery.js');
 
-// Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// Replace the 'ytplayer' element with an <iframe> and
-// YouTube player after the API code downloads.
-var player;
-
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('video-player', {
-        height: '600',
-        width: '800'
-    });
-    player.addEventListener('onStateChange', 'playerStateHandler');
-}
-
-function playerStateHandler(event) {
-    if (event.data == 0) {
-        setTimeout(function(){hideVideoPlayer()},400);
-    }
-}
 
 var checkForBanWord = function (query) {
     return query.split("+").filter(function(chunk) {
@@ -54,13 +31,6 @@ $(document).ready(function () {
     getInstantAnswer(query);
     search(query);
     videoSearch(query);
-
-    $('#modal-backdrop').click(function (e) {
-        hideVideoPlayer();
-    });
-    $('#modal').click(function (e) {
-        e.stopPropagation();
-    });
 });
 
 function getInstantAnswer(query) {
@@ -242,20 +212,4 @@ function scrollStream(resultStream, direction) {
     } else {
         rightNav.show();
     }
-}
-function showVideoPlayer(videoId) {
-    player.loadVideoById({videoId: videoId});
-    showModal();
-}
-
-function hideVideoPlayer(){
-    player.stopVideo();
-    hideModal();
-}
-function showModal() {
-    $('#modal-backdrop').show();
-}
-
-function hideModal() {
-    $('#modal-backdrop').fadeOut();
 }
---
+++ src/video-player.js
@@ -0,0 +1,52 @@
+$(function () {
+    $('#modal-backdrop').click(function (e) {
+        hideVideoPlayer();
+    });
+    $('#modal').click(function (e) {
+        e.stopPropagation();
+    });
+});
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('video-player', {
+        height: '600',
+        width: '800',
+        playerVars: {
+            iv_load_policy: 3
+        }
+    });
+    player.addEventListener('onStateChange', 'playerStateHandler');
+}
+
+function playerStateHandler(event) {
+    if (event.data == 0) {
+        setTimeout(function () {
+            hideVideoPlayer()
+        }, 400);
+    }
+}
+
+function showVideoPlayer(videoId) {
+    player.loadVideoById({videoId: videoId, iv_load_policy: 3});
+    showModal();
+}
+
+function hideVideoPlayer() {
+    player.stopVideo();
+    hideModal();
+}
+
+function showModal() {
+    $('#modal-backdrop').show();
+}
+
+function hideModal() {
+    $('#modal-backdrop').fadeOut();
+}
