27cc900d5dd36af70eb04affefe51f472582f9d4
Anup Vasudevan <anupv@thoughtworks.com>
2016-02-10 12:53:22 +0530

    Add force-yes flag to fix privoxy and monti installation

--- configs/setup-kiosk.sh
+++ configs/setup-kiosk.sh
@@ -1,4 +1,4 @@
-apt-get install -y privoxy
-apt-get install -y monit
+apt-get install -y --force-yes privoxy
+apt-get install -y --force-yes monit
 cp privoxy.monit /etc/monit/conf-enabled/privoxy
 cp default.action.privoxy /etc/privoxy/default.action
