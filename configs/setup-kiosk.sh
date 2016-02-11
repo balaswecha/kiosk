40c919ab1594a623849f607f98ca11afea235dc5
Anup Vasudevan <anupv@thoughtworks.com>
2016-02-11 09:13:38 +0530

    Added custom page for blocked sites

--- configs/setup-kiosk.sh
+++ configs/setup-kiosk.sh
@@ -2,3 +2,4 @@ apt-get install -y --force-yes privoxy
 apt-get install -y --force-yes monit
 cp privoxy.monit /etc/monit/conf-enabled/privoxy
 cp default.action.privoxy /etc/privoxy/default.action
+cp blocked /etc/privoxy/templates/
